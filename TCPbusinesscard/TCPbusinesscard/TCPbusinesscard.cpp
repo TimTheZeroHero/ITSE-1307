3aceba5d99ab937cb96c0164db1d995d912e9614
TimTheZeroHero <47018056+TimTheZeroHero@users.noreply.github.com>
2019-02-07 16:10:46 -0600

    Add files via upload

---
+++ TCPbusinesscard/TCPbusinesscard/TCPbusinesscard.cpp
@@ -0,0 +1,23 @@
+// TCPbusinesscard.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/*
+ * Name:Timothy	Pearson
+ * Section: ITSE 1307
+ * Homework
+ * Description: Creating a business card With 5 lines of details
+ */
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//prints out business info
+	std::cout << endl << "Comany Name: PC Move";
+    std::cout << endl << "Name: Timothy C Pearson";
+	std::cout << endl << "Address: 9207 Ridge Cross";
+	std::cout << endl << "Number: (210)602-3398";
+	std::cout << endl << "E-mail: Tpearson14@student.alamo.edu";
+}
